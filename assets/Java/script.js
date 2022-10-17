55ac9d0490b56b255d9c264cfb16651e27120343
Maryan Abdalla <113936470+maryanne3@users.noreply.github.com>
2022-10-17 10:48:14 +0100

    script.js

--- assets/Java/script.js
+++ assets/Java/script.js
@@ -1,29 +1,34 @@
 // Assignment code here
-function generateBtn = document.querySelector("#generate");
- // Variables + length 
- form.addEventListener('submit' , e => {
-  e.preventDefault()
-  const characterAmount = characterAmountNumber  
-  const password = generatePassword(characterAmount, includeUppercase,
-     includeLowercase, includeNumbers, includeSymbols) 
+var generate = document.querySelector("#generate");
 
- }
+// Function to generate a combination of password    
+function generatePassword() {
+         console.log("Click on the button to generate password");}
+     
+    const string =
+     "abcdefghijklmnopqrstuvwxyz" + "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789" + "~@&#!£$%()*+:";
+   
+    
+ function passwordlength = Number(prompt("Enter password length (8-128):")); 
+    if (passwordlength < 8 || passwordlength > 128) {
+        window.alert ("Please make sure password is between 8 and 128 characters")
+    return passwordlength;
+}   else (passwordlength >= 8 || passwordlength <= 128) {
+    }
+
+ 
+ 
   
-  let lowercase = "abcdefghijklmnopqrstuvwxyz";
-  let uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  let numbers   = '0123456789';
-  let specials  = '~@&#!£$%()*+:;';
-  let password  = "";
-  let character = "";
-  let crunch    = true;
+  
 
-}
 
 
 
 
 
 // Get references to the #generate element
+console.log(generatePassword)
+
 
 
 // Write password to the #password input
@@ -31,8 +36,7 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
-
+      passwordText.value = password;
 }
 
 // Add event listener to generate button
