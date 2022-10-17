75623d388de8afbef7665bbd327082d686496148
Maryan Abdalla <113936470+maryanne3@users.noreply.github.com>
2022-10-17 09:57:47 +0100

    initial

--- assets/Java/script.js
+++ assets/Java/script.js
@@ -1,22 +1,26 @@
 // Assignment code here
-const generateBtn = document.querySelector("#generate");
- // Variables 
- function generatePassword() {
-const lowercase = "abcdefghijklmnopqrstuvwxyz";
-const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const numbers   = "0123456789";
-const specials  = "~@&#!£$%()*+:;";
+function generateBtn = document.querySelector("#generate");
+ // Variables + length 
+ form.addEventListener('submit' , e => {
+  e.preventDefault()
+  const characterAmount = characterAmountNumber  
+  const password = generatePassword(characterAmount, includeUppercase,
+     includeLowercase, includeNumbers, includeSymbols) 
+
+ }
+  
+  let lowercase = "abcdefghijklmnopqrstuvwxyz";
+  let uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  let numbers   = '0123456789';
+  let specials  = '~@&#!£$%()*+:;';
+  let password  = "";
+  let character = "";
+  let crunch    = true;
+
 }
 
-// Password length 
-let passwordlength = 8-128;
-let password = '';
 
-for (passwordlength < 8 || passwordlength > 128) {
-  alert ("choose a different length");
-  return passwordlength;
-} else (passwordlength >=8 || passwordlength <=128){
-}
+
 
 
 // Get references to the #generate element
