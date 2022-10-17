16a5262fda18a2e1715537f2709ce11f8ce3e56a
Maryan Abdalla <113936470+maryanne3@users.noreply.github.com>
2022-10-17 13:29:18 +0100

    password length added

--- assets/Java/script.js
+++ assets/Java/script.js
@@ -1,11 +1,15 @@
 // Assignment code here
-
+// "click" button to generate password
 function generatePassword() {
-  console.log ("Click the button to generate password");
-}
+  console.log ("Click the button to generatepassword");
 
-// Get references to the #generate element
+
+// inserted characters for password
 var generateBtn = document.querySelector("#generate");
+const Uppercase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+const Lowercase = ["abcdefghijklmnopqrstuvwxyz"];
+const Numerics = ["1234567890"];
+const Specials = ["!#$%&()@+=?"];
 
 // Write password to the #password input
 function writePassword() {
@@ -15,5 +19,15 @@ function writePassword() {
       passwordText.value = password;
 }
 
+// password length 
+const length = (prompt("Please enter choice of password length (between 8-128):"));
+if  (length < 8 || length > 128){
+  
+
+}
+
+
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+}
